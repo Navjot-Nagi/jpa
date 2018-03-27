900b462b09696929631cfd797c45b3234726c8de
Navjot-Nagi <31776059+Navjot-Nagi@users.noreply.github.com>
2018-03-27 00:49:18 -0400

    printing
    printing

--- javaExcercise3/JPAExercise3/src/prog3060/nnagi/JPAApplicationExercise3.java
+++ javaExcercise3/JPAExercise3/src/prog3060/nnagi/JPAApplicationExercise3.java
@@ -62,19 +62,36 @@ public class JPAApplicationExercise3 {
             TotalIncome hTotalIncome = tempEntityManager.find(TotalIncome.class, 14);
             
             HouseHold tempHouseHold = new HouseHold();
-            tempHouseHold.setGeographicArea(hGeoArea);
-            tempHouseHold.setCensusYear(hCensusYear);
-            tempHouseHold.setHouseHoldType(hHouseHoldType);
-            tempHouseHold.setHouseHoldSize(hHouseHoldSize);
-            tempHouseHold.setHouseHoldEarners(hHouseHoldsEarners);
-            tempHouseHold.setTotalIncome(hTotalIncome);
-            tempEntityManager.persist(tempHouseHold);
-            tempEntityManager.getTransaction().commit();
-            
-            String houseHoldQuery = "h From HouseHold h "
-            						+"Where h.id = :"
-            
+			tempHouseHold.setGeographicArea(hGeoArea);
+			tempHouseHold.setCensusYear(hCensusYear);
+			tempHouseHold.setHouseHoldType(hHouseHoldType);
+			tempHouseHold.setHouseHoldSize(hHouseHoldSize);
+			tempHouseHold.setHouseHoldsByAgeRange(hHouseHoldsByAgeRange);
+			tempHouseHold.setHouseHoldEarners(hHouseHoldsEarners);
+			tempHouseHold.setTotalIncome(hTotalIncome);
+			tempEntityManager.persist(tempHouseHold);
+
+			String houseHoldQuery = "h From HouseHold h " + "Where h.id = :createdHouseHold";
+			Query hHoldQuery = tempEntityManager.createQuery(houseHoldQuery);
+			hHoldQuery.setParameter("createdHouseHold", tempHouseHold.getId());
+			List<Object[]> tempList = hHoldQuery.getResultList();
+			Iterator<Object[]> iteratorDataPoint = tempList.iterator();
+			Object obj;
+			HouseHold houseHold = new HouseHold();
+
+			while (iteratorDataPoint.hasNext()) {
+				obj = iteratorDataPoint.next();
+				houseHold = (HouseHold) obj;
+				System.out.println("Census Year " + houseHold.getCensusYear().getCensusYear());
+				System.out.println("Geographic area name " + houseHold.getGeographicArea().getName());
+				System.out.println("Geographic area code " + houseHold.getGeographicArea().getCode());
+				System.out.println("Geographic area level " + houseHold.getGeographicArea().getLevel());
+				System.out.println("Geographic area alternative code " + houseHold.getGeographicArea().getAlternativeCode());
+				System.out.println("House holds reported satisfying above the criteria" + houseHold.getNumberReported());
+				System.out.println();
+			}
 		}
+
 		catch(Exception e)
 		{
 			if (tempEntityManager != null)
